de6a56a628a966446b287ec823b01f98431f3045
Evianne Keeney <evianneelise@gmail.com>
2017-03-06 15:47:31 -0500

    This test fails for codeship validation

--- spec/features/failing_test_spec.rb
+++ spec/features/failing_test_spec.rb
@@ -1,1 +1,12 @@
-this is for a failing test
+require 'rails_helper'
+
+# [] Visiting the `/theaters` path should contain a list of theaters.
+
+feature "this test fails" do
+  scenario "this test fails" do
+
+
+    expect(1).to equal(2)
+
+  end
+end
