a57d65c9607748c7ed088bb68c54864156d9ddff
Evianne Keeney <evianneelise@gmail.com>
2017-03-08 12:48:48 -0500

    adds admin privilege  to view user index

--- spec/features/admin_user_index_spec.rb
+++ spec/features/admin_user_index_spec.rb
@@ -8,56 +8,56 @@ feature 'admin sees all users' do
     password: 'supersecret',
     password_confirmation: 'supersecret',
     role: 'member'
-    )}
-    let!(:admin) { User.create(
-      first_name: 'Admin',
-      last_name: 'World',
-      email: 'whatatreet@world.com',
-      password: 'supersecret',
-      password_confirmation: 'supersecret',
-      role: 'admin'
-      )}
+  )}
+  let!(:admin) { User.create(
+    first_name: 'Admin',
+    last_name: 'World',
+    email: 'whatatreet@world.com',
+    password: 'supersecret',
+    password_confirmation: 'supersecret',
+    role: 'admin'
+  )}
 
-      scenario "admin visits user index page through link" do
-        visit root_path
-        click_link "Sign In"
+  scenario "admin visits user index page through link" do
+    visit root_path
+    click_link "Sign In"
 
-        fill_in "Email", with: admin.email
-        fill_in "Password", with: admin.password
-        click_button "Sign In"
+    fill_in "Email", with: admin.email
+    fill_in "Password", with: admin.password
+    click_button "Sign In"
 
-        expect(page).to have_link("All Users")
+    expect(page).to have_link("All Users")
 
-        click_link "All Users"
+    click_link "All Users"
 
-        expect(page).to have_content user.first_name
-      end
+    expect(page).to have_content user.first_name
+  end
 
-      scenario "admin visits the users index page directly" do
-        visit root_path
-        click_link "Sign In"
+  scenario "admin visits the users index page directly" do
+    visit root_path
+    click_link "Sign In"
 
-        fill_in "Email", with: admin.email
-        fill_in "Password", with: admin.password
-        click_button "Sign In"
+    fill_in "Email", with: admin.email
+    fill_in "Password", with: admin.password
+    click_button "Sign In"
 
-        visit admin_users_path
+    visit admin_users_path
 
-        expect(page).to have_content user.first_name
-      end
+    expect(page).to have_content user.first_name
+  end
 
-      scenario "users cannot visit users index page" do
-        visit root_path
-        click_link "Sign In"
+  scenario "users cannot visit users index page" do
+    visit root_path
+    click_link "Sign In"
 
-        fill_in "Email", with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign In"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign In"
 
-        expect(page).to_not have_link("All Users")
+    expect(page).to_not have_link("All Users")
 
-        visit admin_users_path
+    visit admin_users_path
 
-        expect(page).to_not have_content user.first_name
-      end
-    end
+    expect(page).to_not have_content user.first_name
+  end
+end
