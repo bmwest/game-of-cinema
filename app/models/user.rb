6dc8f068911b44a5acba6d1f4e84bd8dd2bab7ea
Evianne Keeney <evianneelise@gmail.com>
2017-03-10 11:26:49 -0500

    WIP Adds a button to upload a file

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :avatar, AvatarUploader
+
+
   def admin?
     role == "admin"
   end
+
 end
