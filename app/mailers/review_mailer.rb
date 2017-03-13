4b4a578a79ce2b88123cf148fe29927197052271
Evianne Keeney <evianneelise@gmail.com>
2017-03-13 15:06:50 -0400

    adds working test for email notifications

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,7 +1,6 @@
 class ReviewMailer < ApplicationMailer
   def new_review(review)
     @review = review
-
    mail(
      to: review.theater.user.email,
      subject: "New Review for #{review.theater.name}"
