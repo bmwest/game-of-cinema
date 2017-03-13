46618912a2f749673dd13cb83df2c9088d9b7fc1
Evianne Keeney <evianneelise@gmail.com>
2017-03-13 16:06:56 -0400

    Adds email notifications to review.save with testing

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,9 +1,9 @@
 class ReviewMailer < ApplicationMailer
   def new_review(review)
     @review = review
-   mail(
-     to: review.theater.user.email,
-     subject: "New Review for #{review.theater.name}"
-   )
+    mail(
+      to: review.theater.user.email,
+      subject: "New Review for #{review.theater.name}"
+    )
   end
 end
