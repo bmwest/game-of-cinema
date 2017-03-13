302797d447cadecc0cf16a7113089327d6aaef5b
bmwest <brianawest.m@gmail.com>
2017-03-13 13:57:06 -0400

    removes theater duplication in app/views/theaters/index.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308154353) do
+ActiveRecord::Schema.define(version: 20170309201337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,8 +46,17 @@ ActiveRecord::Schema.define(version: 20170308154353) do
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
     t.string   "role",                   default: "member", null: false
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "review_id"
+    t.integer  "user_id"
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
