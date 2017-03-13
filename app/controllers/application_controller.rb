e85d1140aacdb907a07b7b8b7bce3232b70671c5
Evianne Keeney <evianneelise@gmail.com>
2017-03-12 23:18:16 -0400

    Adds carrierwave uploader to generate profile avatar using AWS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,26 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [
-      :first_name,
-      :last_name,
-      :password,
-      :password_confirmation,
-      :remember_me,
-      :avatar,
-      :avatar_cache,
-      :remove_avatar
-      ])
-      devise_parameter_sanitizer.permit(:account_update, keys: [
-        :first_name,
-        :last_name,
-        :password,
-        :password_confirmation,
-        :remember_me,
-        :avatar,
-        :avatar_cache,
-        :remove_avatar
-        ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar])
   end
 
   def authenticate_admin!
