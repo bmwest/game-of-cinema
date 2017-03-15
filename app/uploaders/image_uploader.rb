408947a5892fad7f7767c4145eab5f4ca9cc9cc2
bmwest <brianawest.m@gmail.com>
2017-03-15 15:09:46 -0400

    adds theater thumbnail upload feature and default theater photo

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,9 +19,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-  #  # For Rails 3.1+ asset pipeline compatibility:
+   # For Rails 3.1+ asset pipeline compatibility:
     "popcorn.jpg"
-  #  # "/images/fallback/" + [version_name, "movie-theater-revival-popcorn.jpg"].compact.join('_')
+   # "/images/fallback/" + [version_name, "movie-theater-revival-popcorn.jpg"].compact.join('_')
   end
 
   # Process files as they are uploaded:
