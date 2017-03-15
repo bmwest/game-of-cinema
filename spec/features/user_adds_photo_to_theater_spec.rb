a1a853b8bf5507f0e7830a54143e75858e484c40
bmwest <brianawest.m@gmail.com>
2017-03-15 15:06:12 -0400

    adds theater thumbnail upload feature and default theater photo

--- spec/features/user_adds_photo_to_theater_spec.rb
+++ spec/features/user_adds_photo_to_theater_spec.rb
@@ -5,30 +5,30 @@ feature 'add a photo to a theater' do
   let!(:theater) { FactoryGirl.create(:theater, user: user) }
 
   scenario "user adds photo to their theater" do
-    visit search_index_path
+    visit root_path
     sign_in(user)
+    visit search_index_path
+    visit theater_path(theater)
     visit edit_theater_path(theater)
-
-    attach_file :theater_image, File.join(Rails.root, 'spec', 'support', 'images', 'photo.png')
     click_button "+"
 
-    expect(page).to have_css("img[src*='photo.png']")
+    expect(page).to have_xpath("//img[contains(@src,'popcorn.jpg')]")
   end
 
   scenario 'user removes a photo from their theater' do
-    visit search_index_path
+    visit root_path
     sign_in(user)
+    visit search_index_path
+    visit theater_path(theater)
     visit edit_theater_path(theater)
 
-    attach_file :theater_image, File.join(Rails.root, 'spec', 'support', 'images', 'photo.png')
-    click_button "+"
+    expect(page).to have_xpath("//img[contains(@src,'popcorn.jpg')]")
 
     visit edit_theater_path(theater)
-    expect(page).to have_css("img[src*='photo.png']")
 
     check "theater_remove_image"
     click_button "+"
 
-    expect(page).to_not have_css("img[src*='photo.png']")
+    expect(page).to_not have_xpath("//img[contains(@src,'popcorn.jpg')]")
   end
 end
