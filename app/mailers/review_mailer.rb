a4091d7f9600752a2895635b80fe87d69acad2a3
Evianne Keeney <evianneelise@gmail.com>
2017-03-15 10:38:10 -0400

    adds SendGrid functionality for remote email service

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,9 +1,17 @@
 class ReviewMailer < ApplicationMailer
+
   def new_review(review)
     @review = review
     mail(
       to: review.theater.user.email,
       subject: "New Review for #{review.theater.name}"
     )
+  end
+
+  def send_created_theater_email(user)
+    @user = user
+     mail(
+     :to => @user.email,
+     :subject => 'Thanks for adding a theater to our amazing app' )
   end
 end
