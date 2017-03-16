6e14be8c2c78741a6186cfc26224e20fd2b980df
casserni@yahoo.com <nicholascassera@Nicholass-MacBook-Pro.local>
2017-03-16 13:46:30 -0400

    fixes hound violation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, {js_errors: false})
+  Capybara::Poltergeist::Driver.new(app, { js_errors: false })
 end
 
 # Add additional requires below this line. Rails is not loaded until this point!
